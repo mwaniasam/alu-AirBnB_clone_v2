ea47c8e33230e43070bbdfcc4251607c0d790482
mwaniasam <s.mwania@alustudent.com>
2025-01-23 00:30:52 -0800

    Comments

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,14 @@
+-- Create database hbnb_dev_db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Create user hbnb_dev
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant all privileges on hbnb_dev_db to hbnb_dev
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- Grant SELECT privilege on performance_schema to hbnb_dev (and only this database)
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+FLUSH PRIVILEGES;
+```
