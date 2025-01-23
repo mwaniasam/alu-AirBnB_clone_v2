9bec105f2a8c3eb657ea256dff491cb2057ee454
mwaniasam <s.mwania@alustudent.com>
2025-01-22 23:41:10 -0800

    Update the def do_create function to allow for object creation with given parameters

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] =='}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -82,9 +82,10 @@ class HBNBCommand(cmd.Cmd):
             line = ' '.join([_cmd, _cls, _id, _args])
 
         except Exception as mess:
-            pass
+            print(f"** Error: {mess} **")
         finally:
-            return line
+            pass
+        return line
 
     def postcmd(self, stop, line):
         """Prints if isatty is false"""
@@ -115,14 +116,35 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class"""
-        if not args:
+        try:
+            class_name = args.split(' ')[0]
+        except IndexError:
+            pass
+        if not class_name:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+        elif class_name not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
-        storage.save()
+        
+        all_list = args.split(' ')
+
+        new_instance = eval(class_name)()
+
+        for i in range(1, len(all_list)):
+            key, value = tuple(all_list[i].split('='))
+            if value.startswith('"'):
+                value = value.strip('"').replace('_', ' ')
+            else:
+                try:
+                    value = eval(value)
+                except Exception:
+                    print(f'** couldnt evaluate: {value}**')
+                    pass   
+            if hasattr(new_instance, key):
+                setattr(new_instance, key, value)
+
+        storage.new(new_instance)
         print(new_instance.id)
         storage.save()
 
@@ -272,7 +294,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -280,10 +302,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
