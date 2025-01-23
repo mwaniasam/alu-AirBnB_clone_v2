371fcd20e92b55ceef111cb68748ce3ada05a155
mwaniasam <s.mwania@alustudent.com>
2025-01-23 11:43:27 -0800

    Delete a user, delete all user linked cities automatically.

--- models/city.py
+++ models/city.py
@@ -2,7 +2,7 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
-
+from sqlalchemy.orm import relationship
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
@@ -11,3 +11,4 @@ class City(BaseModel, Base):
     
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship("Place", backref="cities", cascade="all, delete")
