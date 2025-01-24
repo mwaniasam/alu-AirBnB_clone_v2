8891e2159ecef9d79857a7a328f40b8a48abc4c5
mwaniasam <s.mwania@alustudent.com>
2025-01-24 02:55:43 -0800

    Updated review

--- models/review.py
+++ models/review.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-""" Review Module for HBNB project """
-from models.base_model import BaseModel, Base
+""" Review module for the HBNB project """
 from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+
+
 
 class Review(BaseModel, Base):
-    """ Review class """
-    __tablename__ = 'reviews'   
+    """ Review class to store review information """
+    __tablename__ = "reviews"
+    id = Column(String(60), primary_key=True)
     text = Column(String(1024), nullable=False)
     place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
