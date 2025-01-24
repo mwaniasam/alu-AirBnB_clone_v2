f47407e11fda662fadd28d08607bca80b68d9427
mwaniasam <s.mwania@alustudent.com>
2025-01-23 21:21:14 -0800

    Delete place, delete review/Storagefile update with getter to return review list

--- models/place.py
+++ models/place.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from sqlalchemy import (Column, String, Float, ForeignKey, Integer)
+from os import getenv
+from sqlalchemy import (Column, String,
+                         ForeignKey, Float,
+                         Integer)
+from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
+import models
+from models.review import Review
 
 
 class Place(BaseModel, Base):
     """ A place to stay """
-    
+
     __tablename__ = 'places'
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
@@ -16,6 +22,17 @@ class Place(BaseModel, Base):
     number_bathrooms = Column(Integer, nullable=False, default=0)
     max_guest = Column(Integer, nullable=False, default=0)
     price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float)
-    longitude = Column(Float)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
+    _reviews = relationship("Review", backref="place", cascade="all, delete")
     amenity_ids = []
+
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def reviews(self):
+            """ Getter attribute that returns the list of Review instances """
+            review_list = []
+            for review in list(models.storage.all(Review).values()):
+                if review.place_id == self.id:
+                    review_list.append(review)
+            return review_list     
