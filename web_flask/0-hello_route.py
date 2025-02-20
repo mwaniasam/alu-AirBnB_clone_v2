6b530d291e88849a2480dfa37dfb6cc48c7d6d17
mwaniasam <s.mwania@alustudent.com>
2025-02-19 22:59:28 -0800

    Document the module

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
+"""
+A simple Flask web application that displays "Hello HBNB!" at the root URL.
+
+This script starts a Flask web server listening on 0.0.0.0 (all available IP addresses)
+on port 5000. When accessed at the root URL (/), it returns the text "Hello HBNB!".
+
+Usage:
+    Run this script directly to start the Flask server:
+        $ python3 app.py
+
+    The server will be accessible at http://0.0.0.0:5000/.
+"""
+
 from flask import Flask
 
 # Create a Flask application
