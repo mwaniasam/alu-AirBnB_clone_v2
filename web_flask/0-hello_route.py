1b752e2f93fcbaca039ac4093917af2947ca14c6
mwaniasam <s.mwania@alustudent.com>
2025-02-19 22:51:43 -0800

    A script that starts a Flask web application.

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from flask import Flask
+
+# Create a Flask application
+app = Flask(__name__)
+
+# Define the route for the root URL (/)
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+# Run the application
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
