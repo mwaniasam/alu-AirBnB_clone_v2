0270405104f01358561bf188b30fb5dc9c33b2da
mwaniasam <s.mwania@alustudent.com>
2025-02-19 23:09:38 -0800

    A script that starts a Flask web application: Display C followed by some text.

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Flask web application that defines multiple routes:
+- `/` displays "Hello HBNB!"
+- `/hbnb` displays "HBNB"
+- `/c/<text>` displays "C " followed by the value of `<text>`, 
+  replacing underscores with spaces.
+The application listens on 0.0.0.0, port 5000.
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Returns the string 'Hello HBNB!'"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Returns the string 'HBNB'"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """Returns 'C ' followed by `text`, replacing underscores with spaces"""
+    return f"C {text.replace('_', ' ')}"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
