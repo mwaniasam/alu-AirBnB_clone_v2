124e62e9b29a28aea1f54a9044184f7bdcc6055c
mwaniasam <s.mwania@alustudent.com>
2025-02-19 22:56:14 -0800

    PEP8 Valid

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 # Define the route for the root URL (/)
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """Display 'Hello HBNB!' at the root URL."""
     return "Hello HBNB!"
 
 # Run the application
